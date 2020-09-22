ac9999860ec342ad530e8860e059e6fc1b8d070f
colin.anderson10 <colin.anderson10@gmail.com>
2020-09-22 12:33:08 -0700

    Assignment2
    sql script and requirements update

--- Assignment2/Assignment2Batting.sql
+++ Assignment2/Assignment2Batting.sql
@@ -1,6 +1,11 @@
 
 
+
+
 USE baseball;
+
+DROP TABLE IF EXISTS totalbattingaverage, annualbattingaverage, rollingbainter, rollingbattingaverage,
+	allbattertotalbattingaverage, allbatterannualbattingaverage, allbatterrollingbainter, allbatterrollingbattingaverage;
 
 #For single batter 112736
 
@@ -24,7 +29,7 @@ CREATE Table annualbattingaverage
 #Intermediate table to help create the rolling batting averages
 
 CREATE Table rollingbainter
-	SELECT bc.game_id, bc.batter, g.local_date as gamedate, DATE_ADD(g.local_date, INTERVAL -100 DAY) as recentdate, bc.Hit as Hit, bc.atBat as atBat
+	SELECT bc.game_id, bc.batter, g.local_date as gamedate, DATE_ADD(g.local_date, INTERVAL -100 DAY) as firstdate, DATE_ADD(g.local_date, INTERVAL -1 DAY) as yesterday, bc.Hit as Hit, bc.atBat as atBat
 	FROM batter_counts bc
 	JOIN game_temp g ON g.game_id=bc.game_id
 	WHERE batter=112736 and atBat>0;
@@ -32,21 +37,13 @@ CREATE Table rollingbainter
 #The rolling batting average over the previous 100 days for each game for 1 player
 	
 CREATE Table rollingbattingaverage
-	SELECT rbi0.batter, rbi0.gamedate, rbi0.recentdate, year(rbi0.gamedate) as gameyear, rbi0.Hit as gamehit, rbi0.atBat as gameatbat, sum(rbi1.Hit) AS rollinghits, sum(rbi1.atBat) AS rollingatbats, sum(rbi1.Hit)/sum(rbi1.atBat) AS rollingbattingaverage
+	SELECT rbi0.batter, rbi0.gamedate, rbi0.firstdate, year(rbi0.gamedate) as gameyear, rbi0.Hit as gamehit, rbi0.atBat as gameatbat, sum(rbi1.Hit) AS rollinghits, sum(rbi1.atBat) AS rollingatbats, sum(rbi1.Hit)/sum(rbi1.atBat) AS rollingbattingaverage
 	FROM rollingbainter rbi0
 	LEFT JOIN rollingbainter rbi1 on rbi0.batter=rbi1.batter
-	WHERE rbi1.gamedate BETWEEN rbi0.recentdate AND rbi0.gamedate
-	GROUP BY batter, gamedate, recentdate, gamehit, gameatbat
-	ORDER BY batter, gamedate, recentdate;
+	WHERE rbi1.gamedate BETWEEN rbi0.firstdate AND rbi0.yesterday
+	GROUP BY batter, gamedate, firstdate, gamehit, gameatbat
+	ORDER BY batter, gamedate, firstdate;
 
-#This table has the rolling, annual, and all time batting average for the one player
-
-CREATE Table single_batter_batting_average
-	SELECT a.batter, a.gamedate, a.gameyear, a.gamehit, a.gameatbat, a.rollinghits, a.rollingatbats, a.rollingbattingaverage, b.annualbattingaverage, c.totalbattingaverage
-	FROM rollingbattingaverage a
-	JOIN annualbattingaverage b ON b.batter=a.batter AND b.gameyear = a.gameyear
-	JOIN totalbattingaverage c on c.batter=a.batter
-	ORDER BY batter, gamedate;
 
 #For all players
 
@@ -63,20 +60,22 @@ CREATE Table allbatterannualbattingaverage
 	WHERE atBat>0
 	GROUP BY batter, gameyear;
 
+#Intermediate table
+
 CREATE Table allbatterrollingbainter
-	SELECT bc.game_id, bc.batter, g.local_date as gamedate, DATE_ADD(g.local_date, INTERVAL -100 DAY) as recentdate, bc.Hit as Hit, bc.atBat as atBat
+	SELECT bc.game_id, bc.batter, g.local_date as gamedate, DATE_ADD(g.local_date, INTERVAL -100 DAY) as firstdate, DATE_ADD(g.local_date, INTERVAL -1 DAY) as yesterday, bc.Hit as Hit, bc.atBat as atBat
 	FROM batter_counts bc
 	JOIN game_temp g ON g.game_id=bc.game_id
 	WHERE atBat>0;
 
-#This table has the rolling, annual, and all time batting average for all players
+#This table has the rolling batting average for all players
 	
 CREATE Table allbatterrollingbattingaverage
-	SELECT rbi0.batter, rbi0.gamedate, rbi0.recentdate, year(rbi0.gamedate) as gameyear, rbi0.Hit as gamehit, rbi0.atBat as gameatbat, sum(rbi1.Hit) AS rollinghits, sum(rbi1.atBat) AS rollingatbats, sum(rbi1.Hit)/sum(rbi1.atBat) AS rollingbattingaverage
+	SELECT rbi0.batter, rbi0.gamedate, rbi0.firstdate, year(rbi0.gamedate) as gameyear, rbi0.Hit as gamehit, rbi0.atBat as gameatbat, sum(rbi1.Hit) AS rollinghits, sum(rbi1.atBat) AS rollingatbats, sum(rbi1.Hit)/sum(rbi1.atBat) AS rollingbattingaverage
 	FROM allbatterrollingbainter rbi0
 	LEFT JOIN allbatterrollingbainter rbi1 on rbi0.batter=rbi1.batter
-	WHERE rbi1.gamedate BETWEEN rbi0.recentdate AND rbi0.gamedate
-	GROUP BY batter, gamedate, recentdate, gamehit, gameatbat
-	ORDER BY batter, gamedate, recentdate;
+	WHERE rbi1.gamedate BETWEEN rbi0.firstdate AND rbi0.yesterday
+	GROUP BY batter, gamedate, firstdate, gamehit, gameatbat
+	ORDER BY batter, gamedate, firstdate;
 
 
