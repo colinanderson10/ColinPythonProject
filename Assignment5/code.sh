e302f6b1f7d30d2e61b932500c9908bd3d27dfbc
colin.anderson10 <colin.anderson10@gmail.com>
2020-11-28 20:55:10 -0800

    Assignment 5 Second Commit
    Checking if database exists

--- Assignment5/code.sh
+++ Assignment5/code.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+if ! mysql -h mysql_db -uroot -psecret -e 'use baseball'; then
+  mysql -h mysql_db -uroot -psecret -e "create database baseball;"
+  mysql -h mysql_db -uroot -psecret -D baseball < /data/baseball.sql
+fi
 
-mysql -h mysql_db -uroot -psecret -e "create database baseball;"
-mysql -h mysql_db -uroot -psecret -D baseball < /data/baseball.sql
 mysql -h mysql_db -uroot -psecret baseball < /scripts/Assignment5Batting.sql
