668411c8f503c9d41b15cb1147bc214f5f5a0a98
Julien Pierret <dafrenchyman@gmail.com>
2020-11-29 13:57:34 -0800

    Great job
    Adding in some fixes that will hopefully make your lief easier for the final. Please review these changes and understand them

--- Assignment5/code.sh
+++ Assignment5/code.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
+sleep 10
+
 if ! mysql -h mysql_db -uroot -psecret -e 'use baseball'; then
   mysql -h mysql_db -uroot -psecret -e "create database baseball;"
   mysql -h mysql_db -uroot -psecret -D baseball < /data/baseball.sql
 fi
 
 mysql -h mysql_db -uroot -psecret baseball < /scripts/Assignment5Batting.sql
+
+mysql -h mysql_db -uroot -psecret baseball -e '
+  SELECT *
+  FROM allbatterrollingbattingaverage;' > /results/all_players_rolling_avg.txt
+
+mysql -h mysql_db -uroot -psecret baseball -e '
+  SELECT *
+  FROM gamerollingbattingaverage;' > /results/game_players_rolling_avg.txt
